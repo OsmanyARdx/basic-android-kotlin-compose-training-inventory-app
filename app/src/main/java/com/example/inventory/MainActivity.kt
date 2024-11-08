f61540cd67cdeac5b1c2151ac5b8dd9a8e485dd7
Osmany A Argueta Claros <osmarguet@gmail.com>
2024-11-08 13:43:01 -0500

    dfgdfg

--- app/src/main/java/com/example/inventory/MainActivity.kt
+++ app/src/main/java/com/example/inventory/MainActivity.kt
@@ -23,7 +23,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.inventory.ui.theme.InventoryTheme
-
+//hjk
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
